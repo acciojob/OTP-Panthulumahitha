6acca9e4e26cc8a6b9846288f174b57218287b44
Panthulumahitha <panthulumahitha0620@gmail.com>
2025-01-29 23:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((code, index) => {
+    code.addEventListener("input", (e) => {
+        if (e.inputType === "deleteContentBackward" && index > 0) {
+            codes[index - 1].focus();
+        } else if (e.target.value && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+    });
+
+    code.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && !code.value && index > 0) {
+            codes[index - 1].focus();
+        }
+    });
+});
+
